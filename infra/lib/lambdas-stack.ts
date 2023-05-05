fa1148fc68ab7643e86809e5db996d7f8cb372e6
Sanfer <dsouz039@umn.edu>
2023-05-05 09:03:28 -0500

    Add permissions to Clipper/Phraser Lambdas

--- infra/lib/lambdas-stack.ts
+++ infra/lib/lambdas-stack.ts
@@ -1,10 +1,11 @@
-import * as path from 'path'
 import * as cdk from 'aws-cdk-lib';
-import * as s3 from 'aws-cdk-lib/aws-s3'
-import * as sqs from 'aws-cdk-lib/aws-sqs'
+import * as iam from 'aws-cdk-lib/aws-iam';
 import * as lambda from 'aws-cdk-lib/aws-lambda'
 import * as lambdaEventSources from 'aws-cdk-lib/aws-lambda-event-sources'
+import * as path from 'path'
 import * as python from '@aws-cdk/aws-lambda-python-alpha'
+import * as s3 from 'aws-cdk-lib/aws-s3'
+import * as sqs from 'aws-cdk-lib/aws-sqs'
 import { Construct } from 'constructs';
 
 const FFMPEG_LAYER_ARN = "arn:aws:lambda:us-east-1:180797159824:layer:ffmpeg:2"
@@ -29,10 +30,12 @@ export class LambdasStack extends cdk.Stack {
         this.clipper = this.createLambdaFunction({ codePath, timeoutDuration, index: "clipper.py", props });
         const ffmpegLayer = lambda.LayerVersion.fromLayerVersionArn(this, "ffmpeg-layer", FFMPEG_LAYER_ARN);
         this.clipper.addLayers(ffmpegLayer);
+        this.addPermissionsToClipper(props.dataBucket, props.clipsQueue)
 
         this.phraser = this.createLambdaFunction({ codePath, timeoutDuration, index: "phraser.py", props });
         const clipsQueueEventSource = new lambdaEventSources.SqsEventSource(props.clipsQueue)
         this.phraser.addEventSource(clipsQueueEventSource)
+        this.addPermissionsToPhraser(props.dataBucket)
   }
 
     private createLambdaFunction({ codePath, timeoutDuration, index, props }: createLambdaFunctionProps) : lambda.Function {
@@ -49,6 +52,47 @@ export class LambdasStack extends cdk.Stack {
                 CLIPS_QUEUE_URL: props.clipsQueue.queueUrl,
             },
         });
+    }
+
+    private addPermissionsToClipper(dataBucket: s3.Bucket, clipsQueue: sqs.Queue): void {
+        this.clipper.addToRolePolicy(new iam.PolicyStatement({
+            actions: ["s3:ListBucket",],
+            resources: [ dataBucket.bucketArn, ]
+        }))
+
+        this.clipper.addToRolePolicy(new iam.PolicyStatement({
+            actions: ["s3:GetObject", "s3:DeleteObject",],
+            resources: [ `${dataBucket.bucketArn}/raw/*`, ]
+        }))
+
+        this.clipper.addToRolePolicy(new iam.PolicyStatement({
+            actions: ["s3:PutObject",],
+            resources: [ `${dataBucket.bucketArn}/clips/*`, ]
+        }))
+
+        this.clipper.addToRolePolicy(new iam.PolicyStatement({
+            actions: ["sqs:SendMessage"],
+            resources: [ clipsQueue.queueArn, ]
+        }))
+    }
+
+    private addPermissionsToPhraser(dataBucket: s3.Bucket): void {
+        this.phraser.addToRolePolicy(new iam.PolicyStatement({
+            actions: ["s3:GetObject", ],
+            resources: [`${dataBucket.bucketArn}/clips/*`],
+        }))
+
+        this.phraser.addToRolePolicy(new iam.PolicyStatement({
+            actions: ["s3:PutObject", ],
+            resources: [`${dataBucket.bucketArn}/phrasers/*`]
+        }))
+
+        this.phraser.addToRolePolicy(new iam.PolicyStatement({
+            actions: ["transcribe:StartTranscriptionJob",
+                      "transcribe:GetTranscriptionJob",
+                      "transcribe:DeleteTranscriptionJob",],
+            resources: ["*"], // wildcard is fine since list-transcription-jobs is DENY
+        }))
     }
 
 }
