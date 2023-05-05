d0e156c850bdbacbbc227491cd560cdd0b799d92
Sanfer <dsouz039@umn.edu>
2023-05-05 09:44:30 -0500

    Prompt for confirmation in helper scripts

--- flatten_data.sh
+++ flatten_data.sh
@@ -3,6 +3,15 @@
 INPUT_DIR="$1"
 OUTPUT_DIR="$2"
 
+echo "input-dir=${INPUT_DIR}"
+echo "output-dir=${OUTPUT_DIR}"
+read -p "Do you wish to continue? " -r
+echo
+if [[ $REPLY =~ ^[Nn]$ ]]
+then
+    exit 1
+fi
+
 mkdir -p $OUTPUT_DIR
 
 # iterate over all the assumed wav files and convert to flat list of wav files with unique integer names
--- upload_files.sh
+++ upload_files.sh
@@ -3,6 +3,16 @@
 BUCKET_NAME=$1
 DIR=$2 
 
+echo "bucket-name=${BUCKET_NAME}"
+echo "data-dir=${DIR}"
+read -p "Do you wish to continue? " -r
+echo
+if [[ $REPLY =~ ^[Nn]$ ]]
+then
+    exit 1
+fi
+
+
 for filepath in $(find $DIR -type f); do
     filename=$(basename $filepath)
     echo aws s3 cp $filepath "s3://$BUCKET_NAME/raw/$filename"
