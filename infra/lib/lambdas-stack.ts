d2b12dd7fab36c7cec71a73474565c543fb87e0d
Sanfer <dsouz039@umn.edu>
2023-05-04 23:15:10 -0500

    Add phraser Lambda with sqs trigger

--- infra/lib/lambdas-stack.ts
+++ infra/lib/lambdas-stack.ts
@@ -3,9 +3,9 @@ import * as cdk from 'aws-cdk-lib';
 import * as s3 from 'aws-cdk-lib/aws-s3'
 import * as sqs from 'aws-cdk-lib/aws-sqs'
 import * as lambda from 'aws-cdk-lib/aws-lambda'
+import * as lambdaEventSources from 'aws-cdk-lib/aws-lambda-event-sources'
 import * as python from '@aws-cdk/aws-lambda-python-alpha'
 import { Construct } from 'constructs';
-import { SqsDestination } from 'aws-cdk-lib/aws-lambda-destinations';
 
 const FFMPEG_LAYER_ARN = "arn:aws:lambda:us-east-1:180797159824:layer:ffmpeg:2"
 
@@ -16,26 +16,47 @@ export interface LambdasStackProps extends cdk.StackProps {
 }
 
 export class LambdasStack extends cdk.Stack {
-  
+
+    readonly clipper: lambda.Function
+    readonly phraser: lambda.Function
+
     constructor(scope: Construct, id: string, props: LambdasStackProps) {
         super(scope, id, props);
 
         const timeoutDuration = cdk.Duration.minutes(   props.visibilityTimeout.toMinutes()/6   )
         const codePath = path.join(__dirname, "..", "..", "app")
 
-        const clipper = new python.PythonFunction(this, "Clipper", {
+        this.clipper = this.createLambdaFunction({ codePath, timeoutDuration, index: "clipper.py", props });
+        const ffmpegLayer = lambda.LayerVersion.fromLayerVersionArn(this, "ffmpeg-layer", FFMPEG_LAYER_ARN);
+        this.clipper.addLayers(ffmpegLayer);
+
+        this.phraser = this.createLambdaFunction({ codePath, timeoutDuration, index: "phraser.py", props });
+        const clipsQueueEventSource = new lambdaEventSources.SqsEventSource(props.clipsQueue)
+        this.phraser.addEventSource(clipsQueueEventSource)
+  }
+
+    private createLambdaFunction({ codePath, timeoutDuration, index, props }: createLambdaFunctionProps) : lambda.Function {
+        const _id = index.split(".")[0]
+        const id = _id[0].toUpperCase() + _id.slice(1, _id.length)
+        return new python.PythonFunction(this, id, {
             entry: codePath,
             runtime: lambda.Runtime.PYTHON_3_10,
             handler: "handle",
-            index: "clipper.py",
+            index,
             timeout: timeoutDuration,
             environment: {
                 BUCKET_NAME: props.dataBucket.bucketName,
                 CLIPS_QUEUE_URL: props.clipsQueue.queueUrl,
             },
-        })
+        });
+    }
 
-        const ffmpegLayer = lambda.LayerVersion.fromLayerVersionArn(this, "ffmpeg-layer", FFMPEG_LAYER_ARN)
-        clipper.addLayers(ffmpegLayer)
-  }
 }
+
+interface createLambdaFunctionProps {
+    codePath: string
+    index: string
+    props: LambdasStackProps
+    timeoutDuration: cdk.Duration
+}
+
