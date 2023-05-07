84a644e620bf5d768a30bfe240287844ee93379f
Sanfer <dsouz039@umn.edu>
2023-05-07 14:28:40 -0500

    Modify clipper to get event from RawQueue

--- app/clipper.py
+++ app/clipper.py
@@ -2,15 +2,16 @@
 The Clipper iterates over all files in specified S3 bucket and clips them with lengths
 3s, 5s, 7s and offsets of floor(clip-length/2).
 """
+from botocore.exceptions import ClientError
 import audio_helpers
 import config
+import json
 import logger
 import pydub
 import s3
+import sqs
 import structlog
 import typing
-import sqs
-from botocore.exceptions import ClientError
 
 
 log = logger.get_log()
@@ -22,29 +23,41 @@ def handle(event, context) -> None:
     raw_s3 = s3.S3(s3.Namespace.RAW)
     clips_s3 = s3.S3(s3.Namespace.CLIPS)
     clips_queue = sqs.SQS(config.CLIPS_QUEUE_URL)
+    raw_queue = sqs.SQS(config.RAW_QUEUE_URL)
 
-    for raw_object_key in raw_s3.list_object_keys():
-        with structlog.contextvars.bound_contextvars(raw_object_s3_path=str(raw_object_key)):
-            try:
-                handle_one_raw_object(raw_s3=raw_s3, clips_s3=clips_s3, clips_queue=clips_queue, raw_object_key=raw_object_key)
-            except ClientError:
-                log.exception("ClientError when handling raw object", exc_info=True)
+    for record in event["Records"]:
+        log.info("Processing record", record=record)
+        body = json.loads(record["body"])
+        receipt_handle = record["receiptHandle"]
+        raw_s3_object_path = body["raw_object_key"]
+        raw_s3_object_key = s3.S3ObjectKey(s3.Namespace.CLIPS, raw_s3_object_path)
+        with structlog.contextvars.bound_contextvars(raw_s3_object_key=str(raw_s3_object_key),
+                                                     receipt_handle=receipt_handle):
+            handle_one_raw_object_key(
+                raw_s3_object_key=raw_s3_object_key,
+                receipt_handle=receipt_handle,
+                raw_s3=raw_s3,
+                clips_s3=clips_s3,
+                raw_queue=raw_queue,
+                clips_queue=clips_queue,
+            )
 
 
-def handle_one_raw_object(raw_s3: s3.S3, clips_s3: s3.S3, clips_queue: sqs.SQS, raw_object_key: s3.S3ObjectKey) -> None:
-    log.info("Getting object")
-    raw_object_bytesio = raw_s3.get_object(raw_object_key)
-    log.info("Successful get object")
+def handle_one_raw_object_key(raw_s3_object_key, receipt_handle, raw_s3,
+                              clips_s3, raw_queue, clips_queue):
+    log.info("Getting raw object")
+    raw_object_bytesio = raw_s3.get_object(raw_s3_object_key)
+    log.info("Successful get raw object")
 
-    log.info("Converting to AudioSegment")
-    audio = pydub.AudioSegment.from_file(raw_object_bytesio, format=raw_object_key.get_extension())
-    log.info("Successful AudioSegment")
+    log.info("Converting raw audio to AudioSegment")
+    audio = pydub.AudioSegment.from_file(raw_object_bytesio, format=raw_s3_object_key.get_extension())
+    log.info("Successful raw audio AudioSegment")
 
-    for (clip_object_key, clip) in generate_audio_clips(raw_object_key.get_filestem(),
-                                                        raw_object_key.get_extension(),
+    for (clip_object_key, clip) in generate_audio_clips(raw_s3_object_key.get_filestem(),
+                                                        raw_s3_object_key.get_extension(),
                                                         audio):
         with structlog.contextvars.bound_contextvars(clip_object_s3_path=str(clip_object_key)):
-            clip_bytesio = audio_helpers.audio_segment_to_bytesio(clip, format=raw_object_key.get_extension())
+            clip_bytesio = audio_helpers.audio_segment_to_bytesio(clip, format=raw_s3_object_key.get_extension())
 
             log.info("Saving clip")
             clips_s3.save(clip_object_key, clip_bytesio)
@@ -56,9 +69,13 @@ def handle_one_raw_object(raw_s3: s3.S3, clips_s3: s3.S3, clips_queue: sqs.SQS, 
                 clips_queue.enqueue(clips_sqs_message)
                 log.info("Successful enqueue clip")
 
-    log.info("Removing object")
-    raw_s3.remove(raw_object_key)
-    log.info("Successful remove object")
+    log.info("Removing raw object")
+    raw_s3.remove(raw_s3_object_key)
+    log.info("Successful remove raw object")
+
+    log.info("Removing message from raw queue")
+    raw_queue.dequeue(receipt_handle)
+    log.info("Removed message from raw queue")
 
 
 def generate_audio_clips(filestem: str, extension: str, audio: pydub.AudioSegment) -> typing.Generator[
